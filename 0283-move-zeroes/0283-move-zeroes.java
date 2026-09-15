748e87537002ff48e28dd3bedf9b761bf688d306
Sandeep Kumar Jaiswal <65164493+jaiswalsandeep508@users.noreply.github.com>
2026-09-15 13:39:20 +0530

    Time: 2 ms (91.71%), Space: 47.5 MB (87.86%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,19 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int insertPos = 0;
+        int j = 0;
 
-        // non-zero elements ko aage laao
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                nums[insertPos] = nums[i];
-                insertPos++;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
             }
-        }
-
-        // remaining positions me 0 bhar do
-        while (insertPos < nums.length) {
-            nums[insertPos] = 0;
-            insertPos++;
         }
     }
 }
